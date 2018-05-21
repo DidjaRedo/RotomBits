edc6bf3e93ba36ec571340f732ba5d2ed8ede0e6
Erik Fortune <pc@erik.org>
2018-05-20 20:43:09 -0700

    moved to jasmine and ES6

--- test/unit/raidCommandsSpec.js
+++ test/unit/raidCommandsSpec.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var CommandProcessor = require("botsbits/lib/commandProcessor");
-var RaidCommands = require("../../lib/raidCommands.js");
+const CommandProcessor = require("botsbits/lib/commandProcessor");
+const RaidCommands = require("../../lib/raidCommands.js");
 
-var eggByStartTimeStrings = [
+const eggByStartTimeStrings = [
     "!add some gym at 11:30 pm",
     "!add some gym at 10:30 am",
     "!add some gym at 9:30",
@@ -13,24 +13,24 @@ var eggByStartTimeStrings = [
     "!add some gym @ 1030",
 ];
 
-var eggByTimerStrings = [
+const eggByTimerStrings = [
     "!add some gym in 20",
     "!add some gym in some town in 20",
 ];
 
-var bossWithTimerStrings = [
+const bossWithTimerStrings = [
     "!add ttar at some gym 20 left",
     "!add ttar at some gym",
     "!add pidgey at some gym 1 left",
 ];
 
-describe("raidCommands", function () {
+describe("raidCommands", () => {
     let cmds = new CommandProcessor();
     RaidCommands.initCommands(cmds);
 
-    describe("egg by start time strings", function () {
-        it("should recognize all valid strings", function () {
-            eggByStartTimeStrings.forEach(function (s) {
+    describe("egg by start time strings", () => {
+        it("should recognize all valid strings", () => {
+            eggByStartTimeStrings.forEach((s) => {
                 let match = cmds.processAll(s);
                 expect(match).toBeDefined();
                 expect(match.length).toBe(1);
@@ -38,9 +38,9 @@ describe("raidCommands", function () {
         });
     });
 
-    describe("egg by timer strings", function () {
-        it("should recognize all valid strings", function () {
-            eggByTimerStrings.forEach(function (s) {
+    describe("egg by timer strings", () => {
+        it("should recognize all valid strings", () => {
+            eggByTimerStrings.forEach((s) => {
                 let match = cmds.processAll(s);
                 expect(match).toBeDefined();
                 expect(match.length).toBe(1);
@@ -48,9 +48,9 @@ describe("raidCommands", function () {
         });
     });
 
-    describe("boss with timer strings", function () {
-        it("should recognize all valid strings", function () {
-            bossWithTimerStrings.forEach(function (s) {
+    describe("boss with timer strings", () => {
+        it("should recognize all valid strings", () => {
+            bossWithTimerStrings.forEach((s) => {
                 let match = cmds.processAll(s);
                 expect(match).toBeDefined();
                 expect(match.length).toBe(1);
